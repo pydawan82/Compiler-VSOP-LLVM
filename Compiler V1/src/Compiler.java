2a9490fb638f60efa4f3db7e64b4a5f3cdbc24a0
David Birtles <dbirtles@hotmail.com>
2021-02-10 21:56:57 +0100

    Added correct format for error handling.

--- Compiler V1/src/Compiler.java
+++ Compiler V1/src/Compiler.java
@@ -1,8 +1,13 @@
 import java.io.IOException;
 import java.io.PrintStream;
 
+import org.antlr.v4.runtime.ANTLRErrorStrategy;
+import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.ConsoleErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
 import org.antlr.v4.runtime.Token;
 
 public class Compiler {
@@ -13,10 +18,21 @@ public class Compiler {
 	
 	public Compiler(String fName) throws IOException {
 		CharStream input = CharStreams.fromFileName("vsop/main.vsop");
+		ANTLRErrorStrategy strategy;
 		lexer = new VSOPLexer(input);
+		
+		lexer.removeErrorListener(ConsoleErrorListener.INSTANCE);
+		lexer.addErrorListener(new BaseErrorListener() {
+			@Override
+			public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line,
+					int charPositionInLine, String msg, RecognitionException e) {
+				printError(line, charPositionInLine+1, msg);
+			}
+		});
+		
 	}
 	
-	public void compile() {
+	public void lex() {
 		while(true) {
 			Token t = lexer.nextToken();
 			if(t.getType() == -1)
@@ -32,12 +48,13 @@ public class Compiler {
 			out.println();
 	}
 	
-	private void printError() {
-		err.printf("");
+	private void printError(int ln, int cl, String msg) {
+		err.printf("%d:%d: lexical error: %s", ln, cl, msg);
+		err.println();
 	}
 	
 	public static void main(String[] args) throws IOException {
 		Compiler c = new Compiler("vsop/main.vsop");
-		c.compile();
+		c.lex();
 	}
 }
