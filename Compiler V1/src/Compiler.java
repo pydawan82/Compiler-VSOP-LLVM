4da55e76df9b0d5286a8a48b3f4f709b3f9532b2
David Birtles <dbirtles@hotmail.com>
2021-02-10 13:43:01 +0100

    Outputs token on the output

--- Compiler V1/src/Compiler.java
+++ Compiler V1/src/Compiler.java
@@ -1,7 +1,39 @@
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Token;
 
 public class Compiler {
 
-	public static void main(String[] args) {
+	PrintStream out = System.out;
+	PrintStream err = System.err;
+	
+	public Compiler(String fName) throws IOException {
+		CharStream input = new ANTLRFileStream(fName);
+		VSOPLexer lexer = new VSOPLexer(input);
 		
+		while(true) {
+			Token t = lexer.nextToken();
+			if(t.getType() == -1)
+				break;
+			print(t.getLine(), t.getCharPositionInLine()+1, ""+t.getType(), t.getText());
+		}
+	}
+	
+	private void print(int ln, int cl, String type, String text) {
+			out.printf("%d,%d,%s", ln, cl, type);
+			if(text != null)
+				out.printf(",%s", text);
+			out.println();
+	}
+	
+	private void printError() {
+		err.printf("");
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Compiler c = new Compiler("vsop/main.vsop");
 	}
 }
