9227f06a48c842bda4d50da167e51d142d1499d9
David Birtles <dbirtles@hotmail.com>
2021-02-10 18:15:08 +0100

    Improved

--- Compiler V1/src/Compiler.java
+++ Compiler V1/src/Compiler.java
@@ -1,19 +1,22 @@
 import java.io.IOException;
 import java.io.PrintStream;
 
-import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
 
 public class Compiler {
 
 	PrintStream out = System.out;
 	PrintStream err = System.err;
+	VSOPLexer lexer;
 	
 	public Compiler(String fName) throws IOException {
-		CharStream input = new ANTLRFileStream(fName);
-		VSOPLexer lexer = new VSOPLexer(input);
-		
+		CharStream input = CharStreams.fromFileName("vsop/main.vsop");
+		lexer = new VSOPLexer(input);
+	}
+	
+	public void compile() {
 		while(true) {
 			Token t = lexer.nextToken();
 			if(t.getType() == -1)
@@ -35,5 +38,6 @@ public class Compiler {
 	
 	public static void main(String[] args) throws IOException {
 		Compiler c = new Compiler("vsop/main.vsop");
+		c.compile();
 	}
 }
