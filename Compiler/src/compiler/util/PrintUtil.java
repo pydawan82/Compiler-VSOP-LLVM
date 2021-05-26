a64dca8999fb87501c58e9cc81e4b86de6e3f3e7
David Birtles <dbirtles@hotmail.com>
2021-05-27 00:40:46 +0200

    Added join

--- Compiler/src/compiler/util/PrintUtil.java
+++ Compiler/src/compiler/util/PrintUtil.java
@@ -1,6 +1,7 @@
 package compiler.util;
 
 import java.io.PrintStream;
+import java.util.Iterator;
 import java.util.MissingFormatArgumentException;
 import java.util.UnknownFormatConversionException;
 import java.util.function.Consumer;
@@ -12,6 +13,7 @@ public final class PrintUtil {
     private static final Pattern pattern = Pattern.compile("%(.)");
 
     private static final String print = "p";
+    private static final String newline = "n";
     private static final String percent = "%";
 
     @SafeVarargs
@@ -34,6 +36,10 @@ public final class PrintUtil {
                     i++;
                     break;
                 
+                case newline:
+                    stream.println();
+                    break;
+                
                 case percent:
                     stream.print(percent);
                     break;
@@ -46,6 +52,34 @@ public final class PrintUtil {
         }
 
         stream.print(format.substring(begin));
+    }
+
+    @SafeVarargs
+    public static void join(PrintStream stream, String separator, Consumer<PrintStream> ... printers) {
+        int i = 0;
+
+        for(var printer: printers) {
+            i++;
+            printer.accept(stream);
+            if(i < printers.length)
+                stream.print(separator);
+        }
+    }
+
+    public static void join(PrintStream stream, String separator, Iterable<Consumer<PrintStream>> printers) {
+        Iterator<Consumer<PrintStream>> it = printers.iterator();
+
+        if(!it.hasNext())
+            return;
+
+        while(true) {
+            it.next().accept(stream);
+            
+            if(it.hasNext())
+                stream.print(separator);
+            else
+                break;
+        }
     }
 
 }
