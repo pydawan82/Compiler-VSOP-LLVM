2fccdaf549d9472e222e75f5e5278c6991d1853b
David Birtles <dbirtles@hotmail.com>
2021-05-27 00:12:10 +0200

    Added class to format efficiently print streams

---
+++ Compiler/src/compiler/util/PrintUtil.java
@@ -0,0 +1,51 @@
+package compiler.util;
+
+import java.io.PrintStream;
+import java.util.MissingFormatArgumentException;
+import java.util.UnknownFormatConversionException;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class PrintUtil {
+
+    private static final Pattern pattern = Pattern.compile("%(.)");
+
+    private static final String print = "p";
+    private static final String percent = "%";
+
+    @SafeVarargs
+    public static void format(PrintStream stream, String format, Consumer<PrintStream> ... printers) {
+        Matcher matcher = pattern.matcher(format);
+        int i = 0;
+        int begin = 0;
+        int end = 0;
+
+        while(matcher.find()) {
+            end = matcher.start();
+            stream.print(format.substring(begin, end));
+
+            String option = matcher.group(1);
+            switch(option) {
+                case print:
+                    if(i >= printers.length)
+                        throw new MissingFormatArgumentException("%p");
+                    printers[i].accept(stream);
+                    i++;
+                    break;
+                
+                case percent:
+                    stream.print(percent);
+                    break;
+                
+                default:
+                    throw new UnknownFormatConversionException(option);
+            }
+
+            begin = matcher.end();
+        }
+
+        stream.print(format.substring(begin));
+    }
+
+}
