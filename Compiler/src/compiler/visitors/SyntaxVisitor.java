63bfb94dcb227939714fff5816a24e5d9bace39e
David Birtles <dbirtles@hotmail.com>
2021-05-27 02:55:58 +0200

    static methods, clarified formats, prettier print

--- Compiler/src/compiler/visitors/SyntaxVisitor.java
+++ Compiler/src/compiler/visitors/SyntaxVisitor.java
@@ -1,13 +1,15 @@
 package compiler.visitors;
 
 import java.io.PrintStream;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
+import org.antlr.v4.runtime.ParserRuleContext;
+
 import compiler.parsing.VSOPParser.*;
+import compiler.vsop.VSOPConstants;
 
 import static compiler.util.PrintUtil.*;
 
@@ -16,57 +18,63 @@ import static compiler.util.PrintUtil.*;
  */
 public class SyntaxVisitor {
 
-	private static final String TAB = "\t";
+	private static final String TAB = " ";
 
-	private final PrintStream out;
-	private final Map<Class<?>, Consumer<ExprContext>> map = new HashMap<>();
-	private final String defaultClazz = "Object";
+	private static final Map<Class<?>, BiConsumer<PrintStream, ParserRuleContext>> dispatcher;
 
-	private int tab = -1;
+	private static int tab = 0;
 
-	public SyntaxVisitor() {
-		this(System.out);
+	static {
+
+		dispatcher = Map.ofEntries(
+			Map.entry(ProgramContext.class, (out, ctx) -> print(out, (ProgramContext) ctx)),
+			Map.entry(ClazzContext.class, (out, ctx) -> print(out, (ClazzContext) ctx)),
+			Map.entry(ClassBodyContext.class, (out, ctx) -> print(out, (ClassBodyContext)ctx)),
+			Map.entry(FieldContext.class, (out, ctx) -> print(out, (FieldContext) ctx)),
+			Map.entry(MethodContext.class, (out, ctx) -> print(out, (MethodContext) ctx)),
+			Map.entry(FormalsContext.class, (out, ctx) -> print(out, (FormalsContext) ctx)),
+			Map.entry(FormalContext.class, (out, ctx) -> print(out, (FormalContext) ctx)),
+			Map.entry(BlockContext.class, (out, ctx) -> print(out, (BlockContext) ctx)),
+			Map.entry(IfContext.class, (out, ctx) -> print(out, (IfContext) ctx)),
+			Map.entry(WhileContext.class, (out, ctx) -> print(out, (WhileContext) ctx)),
+			Map.entry(LetContext.class, (out, ctx) -> print(out, (LetContext) ctx)),
+			Map.entry(AssContext.class, (out, ctx) -> print(out, (AssContext) ctx)),
+			Map.entry(NotContext.class, (out, ctx) -> print(out, (NotContext) ctx)),
+			Map.entry(BinopContext.class, (out, ctx) -> print(out, (BinopContext) ctx)),
+			Map.entry(MinusContext.class, (out, ctx) -> print(out, (MinusContext) ctx)),
+			Map.entry(IsnullContext.class, (out, ctx) -> print(out, (IsnullContext) ctx)),
+			Map.entry(SelfcallContext.class, (out, ctx) -> print(out, (SelfcallContext) ctx)),
+			Map.entry(CallContext.class, (out, ctx) -> print(out, (CallContext) ctx)),
+			Map.entry(NewContext.class, (out, ctx) -> print(out, (NewContext) ctx)),
+			Map.entry(OiContext.class, (out, ctx) -> print(out, (OiContext) ctx)),
+			Map.entry(LitContext.class, (out, ctx) -> print(out, (LitContext) ctx)),
+			Map.entry(SelfContext.class, (out, ctx) -> print(out, (SelfContext) ctx)),
+			Map.entry(UnitContext.class, (out, ctx) -> print(out, (UnitContext) ctx)),
+			Map.entry(BraceExprContext.class, (out, ctx) -> print(out, (BraceExprContext) ctx)),
+			Map.entry(BlContext.class, (out, ctx) -> print(out, (BlContext) ctx))
+		);
 	}
 
-	public SyntaxVisitor(PrintStream out) {
-		this.out = Objects.requireNonNull(out);
+	private static String listFormat = "[#n%s#p#n%s]";
+	private static String emptyList = "[]";
+	private static String listSep = ","+System.lineSeparator();
 
-		map.put(IfContext.class, (ExprContext c) -> visitIf((IfContext) c));
-		map.put(WhileContext.class, (ExprContext c) -> visitWhile((WhileContext) c));
-		map.put(LetContext.class, (ExprContext c) -> visitLet((LetContext) c));
-		map.put(AssContext.class, (ExprContext c) -> visitAss((AssContext) c));
-		map.put(NotContext.class, (ExprContext c) -> visitNot((NotContext) c));
-		map.put(BinopContext.class, (ExprContext c) -> visitBinop((BinopContext) c));
-		map.put(MinusContext.class, (ExprContext c) -> visitMinus((MinusContext) c));
-		map.put(IsnullContext.class, (ExprContext c) -> visitIsnull((IsnullContext) c));
-		map.put(SelfcallContext.class, (ExprContext c) -> visitSelfcall((SelfcallContext) c));
-		map.put(CallContext.class, (ExprContext c) -> visitCall((CallContext) c));
-		map.put(NewContext.class, (ExprContext c) -> visitNew((NewContext) c));
-		map.put(OiContext.class, (ExprContext c) -> visitOi((OiContext) c));
-		map.put(LitContext.class, (ExprContext c) -> visitLit((LitContext) c));
-		map.put(SelfContext.class, (ExprContext c) -> visitSelf((SelfContext) c));
-		map.put(UnitContext.class, (ExprContext c) -> visitUnit((UnitContext) c));
-		map.put(BraceExprContext.class, (ExprContext c) -> visitBraceExpr((BraceExprContext) c));
-		map.put(BlContext.class, (ExprContext c) -> visitBl((BlContext) c));
-	}
+	private static <Context extends ParserRuleContext> void printList(PrintStream out, List<Context> contexts) {
+		List<Consumer<PrintStream>> printers = contexts.stream()
+				.map(c -> (Consumer<PrintStream>) (stream -> dispatch(stream, c)))
+				.toList();
 
-	/**
-	 * Print tabulation in order to make the output a bit more readable.
-	 */
-	private void printTab() {
-		for (int i = 0; i < tab; i++) {
-			out.print(TAB);
+
+		if(printers.size() == 0) {
+			out.print(emptyList);
+		} else {
+			String format = listFormat.formatted(TAB.repeat(tab), TAB.repeat(tab));
+			pformat(out, format, (stream) -> join(stream, listSep+TAB.repeat(tab), printers));
 		}
 	}
 
-	private static String listFormat = "[%p]";
-	private static String listSep = ","+System.lineSeparator();
-	/**
-	 * 
-	 * @param printers
-	 */
-	private void printList(PrintStream out, List<Consumer<PrintStream>> printers) {
-		format(out, listFormat, (stream) -> join(stream, listSep, printers));
+	private static void dispatch(PrintStream out, ParserRuleContext ctx) {
+		dispatcher.get(ctx.getClass()).accept(out, ctx);
 	}
 
 
@@ -74,319 +82,240 @@ public class SyntaxVisitor {
 	 * Print out the AST for the -c options of the compiler using the visitor methodology.
 	 * @param ctx - the program context.
 	 */
-	public void visitProgram(ProgramContext ctx, PrintStream out) {
+	public static void print(PrintStream out, ProgramContext ctx) {
 		tab++;
 
-		List<Consumer<PrintStream>> printers = ctx.clazz().stream()
-				.map(c -> (Consumer<PrintStream>) (stream -> visitClazz(c, stream)))
-				.toList();
-
-		printList(out, printers);
+		printList(out, ctx.clazz());
 
 		tab--;
 	}
 
-	public void visitClazz(ClazzContext ctx, PrintStream out) {
+	public static void print(PrintStream out, ClazzContext ctx) {
 		tab++;
+
+		String id = ctx.id.getText();
+		String superId = ctx.idext != null ? ctx.idext.getText() : VSOPConstants.OBJECT.id;
+		String format = "Class(%s, %s, #p)".formatted(id, superId);
+
+		pformat(out, format, (stream) -> print(stream, ctx.classBody()));
+
+		tab--;
+	}
+
+	public static void print(PrintStream out, ClassBodyContext ctx) {
+		tab++;
+
+		String format = "#p, #p";
+
+		pformat(out, format,
+			stream -> printList(stream, ctx.field()),
+			stream -> printList(stream, ctx.method())
+		);
+
+		tab--;
+	}
+
+	public static void print(PrintStream out, FieldContext ctx) {
+		tab++;
+
+		String id = ctx.id.getText();
+
+		if(ctx.expr() == null) {
+			String format  = "Field(%s, #p)".formatted(id);
+
+			pformat(out, format, stream -> print(stream, ctx.type()));
+		} else {
+			String format  = "Field(%s, #p, #p)".formatted(id);
+
+			pformat(out, format,
+				stream -> print(stream, ctx.type()),
+				stream -> dispatchExpr(stream, ctx.expr())
+			);
+		}
+		tab--;
+	}
+
+	public static void print(PrintStream out, MethodContext ctx) {
+		tab++;
+
+		String id = ctx.id.getText();
+		String format = "Method(%s, #p, #p, #p)".formatted(id);
+
+		pformat(out, format,
+				stream -> print(out, ctx.formals()),
+				stream -> print(out, ctx.type()),
+				stream -> print(out, ctx.block())
+			);
+
+		tab--;
+	}
+
+	public static void print(PrintStream out, FormalsContext ctx) {
+		printList(out, ctx.formal());
+	}
+
+	public static void print(PrintStream out, FormalContext ctx) {
+		String id = ctx.id.getText();
+		String format = "%s: #p".formatted(id);
+
+		pformat(out, format, stream -> print(stream, ctx.type()));
+	}
+
+	public static void print(PrintStream out, BlockContext ctx) {
+		tab++;
+
+		printList(out, ctx.expr());
+
+		tab--;
+	}
+
+	public static void dispatchExpr(PrintStream out, ExprContext ctx) {
+		dispatch(out, ctx);
+	}
+
+	public static void print(PrintStream out, ArgsContext ctx) {
+		tab++;
+
+		printList(out, ctx.expr());
+
+		tab--;
+	}
+
+	public static void print(PrintStream out, LiteralContext ctx) {
+		out.print(ctx.getText());
+	}
+
+	public static void print(PrintStream out, BooleanLiteralContext ctx) {
+		out.print(ctx.getText());
+	}
+
+	public static void print(PrintStream out, TypeContext ctx) {
+		out.print(ctx.getText());
+	}
+
+	public static void print(PrintStream out, AssContext ctx) {
+		String id = ctx.id.getText();
+		String format = "Assign(%s, #p)".formatted(id);
+		pformat(out, format, stream -> dispatchExpr(stream, ctx.expr())); 
+	}
+
+	public static void print(PrintStream out, NewContext ctx) {
+		String id = ctx.id.getText();
+		out.printf("New(%s)", id);
+	}
+
+	public static void print(PrintStream out, BlContext ctx) {
+		print(out, ctx.block());
+	}
+
+	public static void print(PrintStream out, WhileContext ctx) {
+		String format = "While(#p, #p)";
 		
-		out.printf("Class(%s, %s, ", ctx.id.getText(), ctx.idext != null ? ctx.idext.getText() : defaultClazz);
-
-		visitClassBody(ctx.classBody());
-
-		out.print(')');
-
-		tab--;
+		pformat(out, format, 
+			stream -> dispatchExpr(stream, ctx.expr(0)),
+			stream -> dispatchExpr(stream, ctx.expr(1))
+		);
 	}
 
-	public void visitClassBody(ClassBodyContext ctx) {
-		tab++;
-
-		out.print('[');
-		int i = 0;
-		for (var field : ctx.field()) {
-			i++;
-			out.println();
-			printTab();
-			visitField(field);
-
-			if (i != ctx.field().size()) {
-				out.print(",");
-			}
-		}
-		out.print("], ");
-
-		if (ctx.field().size() != 0) {
-			out.println();
-			printTab();
-		}
-
-		out.print('[');
-
-		i = 0;
-		for (var method : ctx.method()) {
-			i++;
-			out.println();
-			printTab();
-			visitMethod(method);
-
-			if (i != ctx.method().size()) {
-				out.print(",");
-			}
-		}
-		out.print(']');
-
-		tab--;
+	public static void print(PrintStream out, NotContext ctx) {
+		String format = "UnOp(not, #p)";
+		
+		pformat(out, format, stream -> dispatchExpr(out, ctx.expr()));
 	}
 
-	public void visitField(FieldContext ctx) {
-		tab++;
-
-		out.printf("Field(%s, %s", ctx.id.getText(), ctx.type().getText());
-
-		if (ctx.expr() != null) {
-			out.print(", ");
-			visitExpr(ctx.expr());
-		}
-
-		out.print(')');
-		tab--;
+	public static void print(PrintStream out, MinusContext ctx) {
+		String format = "UnOp(-, #p)";
+		
+		pformat(out, format, stream -> dispatchExpr(out, ctx.expr()));
 	}
 
-	public void visitMethod(MethodContext ctx) {
-		tab++;
-
-		out.printf("Method(%s, ", ctx.id.getText());
-
-		visitFormals(ctx.formals());
-
-		out.print(", ");
-
-		visitType(ctx.type());
-		out.print(", ");
-
-		visitBlock(ctx.block());
-
-		out.print(')');
-
-		tab--;
+	public static void print(PrintStream out, IsnullContext ctx) {
+		String format = "UnOp(isnull, #p)";
+		
+		pformat(out, format, stream -> dispatchExpr(out, ctx.expr()));
 	}
 
-	public void visitFormals(FormalsContext ctx) {
-		out.print('[');
-
-		int i = 0;
-		for (var formal : ctx.formal()) {
-			i++;
-
-			out.println();
-			printTab();
-
-			visitFormal(formal);
-
-			if (i != ctx.formal().size()) {
-				out.print(",");
-			}
-		}
-
-		if (ctx.formal().size() != 0) {
-			out.println();
-			printTab();
-		}
-
-		out.print(']');
+	public static void print(PrintStream out, SelfcallContext ctx) {
+		String id = ctx.id.getText();
+		String  format = "Call(self, %s, #p)".formatted(id);
+		
+		pformat(out, format, stream -> print(stream, ctx.args()));
 	}
 
-	public void visitFormal(FormalContext ctx) {
-		out.print(ctx.id.getText() + " : ");
-		visitType(ctx.type());
+	public static void print(PrintStream out, CallContext ctx) {
+		String id = ctx.id.getText();
+		String  format = "Call(#p, %s, #p)".formatted(id);
+		
+		pformat(out, format,
+			stream -> dispatchExpr(stream, ctx.expr()),
+			stream -> print(stream, ctx.args())
+		);
 	}
 
-	public void visitBlock(BlockContext ctx) {
-		tab++;
-
-		out.print('[');
-
-		int i = 0;
-		for (var expr : ctx.expr()) {
-			i++;
-
-			out.println();
-			printTab();
-
-			visitExpr(expr);
-
-			if (i != ctx.expr().size()) {
-				out.print(",");
-			}
-		}
-
-		if (ctx.expr().size() != 0) {
-			out.println();
-			printTab();
-		}
-
-		out.print(']');
-
-		tab--;
-	}
-
-	public void visitExpr(ExprContext ctx) {
-		map.get(ctx.getClass()).accept(ctx);
-	}
-
-	public void visitArgs(ArgsContext ctx) {
-		tab++;
-
-		out.print("[");
-
-		int i = 0;
-		for (var expr : ctx.expr()) {
-			i++;
-
-			out.println();
-			printTab();
-
-			visitExpr(expr);
-
-			if (i != ctx.expr().size()) {
-				out.print(",");
-			}
-		}
-
-		if (ctx.expr().size() != 0) {
-			out.println();
-			printTab();
-		}
-
-		out.print("]");
-
-		tab--;
-	}
-
-	public void visitLiteral(LiteralContext ctx) {
-		out.print(ctx.getText());
-	}
-
-	public void visitBooleanLiteral(BooleanLiteralContext ctx) {
-		out.print(ctx.getText());
-	}
-
-	public void visitType(TypeContext ctx) {
-		out.print(ctx.getText());
-	}
-
-	public void visitAss(AssContext ctx) {
-		out.print("Assign(" + ctx.id.getText() + ", ");
-		visitExpr(ctx.expr());
-		out.print(")");
-	}
-
-	public void visitNew(NewContext ctx) {
-		out.print("New(" + ctx.id.getText() + ")");
-	}
-
-	public void visitBl(BlContext ctx) {
-		visitBlock(ctx.block());
-	}
-
-	public void visitWhile(WhileContext ctx) {
-		out.print("While(");
-		visitExpr(ctx.expr(0));
-		out.print(", ");
-		visitExpr(ctx.expr(1));
-		out.print(")");
-	}
-
-	public void visitNot(NotContext ctx) {
-		out.print("UnOp(not, ");
-		visitExpr(ctx.expr());
-		out.print(")");
-	}
-
-	public void visitMinus(MinusContext ctx) {
-		out.print("UnOp(-, ");
-		visitExpr(ctx.expr());
-		out.print(")");
-	}
-
-	public void visitIsnull(IsnullContext ctx) {
-		out.print("UnOp(isnull, ");
-		visitExpr(ctx.expr());
-		out.print(")");
-	}
-
-	public void visitSelfcall(SelfcallContext ctx) {
-		out.print("Call(self, " + ctx.id.getText() + ", ");
-
-		visitArgs(ctx.args());
-
-		out.print(')');
-	}
-
-	public void visitCall(CallContext ctx) {
-		out.print("Call(");
-
-		visitExpr(ctx.expr());
-
-		out.print(", " + ctx.id.getText() + ", ");
-
-		visitArgs(ctx.args());
-
-		out.print(')');
-	}
-
-	public void visitUnit(UnitContext ctx) {
+	public static void print(PrintStream out, UnitContext ctx) {
 		out.print("()");
 	}
 
-	public void visitLit(LitContext ctx) {
-		visitLiteral(ctx.literal());
+	public static void print(PrintStream out, LitContext ctx) {
+		print(out, ctx.literal());
 	}
 
-	public void visitSelf(SelfContext ctx) {
+	public static void print(PrintStream out, SelfContext ctx) {
 		out.print("self");
 	}
 
-	public void visitLet(LetContext ctx) {
-		out.print("Let(" + ctx.id.getText() + ", ");
-		visitType(ctx.type());
-		out.print(", ");
+	public static void print(PrintStream out, LetContext ctx) {
+		String id = ctx.id.getText();
 
-		if (ctx.as != null) {
-			visitExpr(ctx.as);
-			out.print(", ");
+		if(ctx.as == null) {
+			String format = "Let(%s, #p, #p)".formatted(id);
+			pformat(out, format,
+				stream -> print(stream, ctx.type()),
+				stream -> dispatchExpr(stream, ctx.ex)
+			);
+		} else {
+			String format = "Let(%s, #p, #p, #p)".formatted(id);
+			pformat(out, format,
+				stream -> print(stream, ctx.type()),
+				stream -> dispatchExpr(stream, ctx.as),
+				stream -> dispatchExpr(stream, ctx.ex)
+			);
 		}
-		visitExpr(ctx.ex);
-		out.print(")");
 	}
 
-	public void visitOi(OiContext ctx) {
+	public static void print(PrintStream out, OiContext ctx) {
 		out.print(ctx.id.getText());
 	}
 
-	public void visitIf(IfContext ctx) {
-		out.print("If(");
-		visitExpr(ctx.expr(0));
-		out.print(", ");
-		visitExpr(ctx.expr(1));
+	public static void print(PrintStream out, IfContext ctx) {
 
-		if (ctx.expr(2) != null) {
-			out.print(", ");
-			visitExpr(ctx.expr(2));
+		if(ctx.expr(2) == null) {
+			String format = "If(#p, #p)";
+			pformat(out, format,
+				stream -> dispatchExpr(stream, ctx.expr(0)),
+				stream -> dispatchExpr(stream, ctx.expr(1))
+			);
+		} else {
+			String format = "If(#p, #p, #p)";
+			pformat(out, format,
+				stream -> dispatchExpr(stream, ctx.expr(0)),
+				stream -> dispatchExpr(stream, ctx.expr(1)),
+				stream -> dispatchExpr(stream, ctx.expr(2))
+			);
 		}
-
-		out.print(")");
 	}
 
-	public void visitBraceExpr(BraceExprContext ctx) {
-		visitExpr(ctx.expr());
+	public static void print(PrintStream out, BraceExprContext ctx) {
+		dispatchExpr(out, ctx.expr());
 	}
 
-	public void visitBinop(BinopContext ctx) {
-		out.print("BinOp(" + ctx.op.getText() + ", ");
-		visitExpr(ctx.expr(0));
-		out.print(", ");
-		visitExpr(ctx.expr(1));
-		out.print(")");
+	public static void print(PrintStream out, BinopContext ctx) {
+		String op = ctx.op.getText();
+		String format = "BinOp(%s, #p, #p)".formatted(op);
+		
+		pformat(out, format, 
+			stream -> dispatchExpr(out, ctx.expr(0)),
+			stream -> dispatchExpr(out, ctx.expr(1))
+		);
 	}
 }
